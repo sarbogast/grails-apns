e2ae7a8a88fc5a8583d419cd79f0481f4dde1c70
Burt Beckwith <burt@burtbeckwith.com>
2016-09-08 09:20:16 -0400

    cleanup

--- ApnsGrailsPlugin.groovy
+++ ApnsGrailsPlugin.groovy
@@ -2,65 +2,41 @@ import org.epseelon.grails.apns.ApnsFactoryBean
 import grails.util.Environment
 
 class ApnsGrailsPlugin {
-    // the plugin version
     def version = "1.2"
-    // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.3.8 > *"
-    // the other plugins this plugin depends on
-    def dependsOn = [:]
-    // resources that are excluded from plugin packaging
-    def pluginExcludes = [
-            "grails-app/views/error.gsp"
+    def grailsVersion = "2.3 > *"
+    def title = "Apple Push Notification Service Plugin"
+    def author = "Sebastien Arbogast"
+    def authorEmail = "sebastien.arbogast@gmail.com"
+    def description = 'Integrates with Apple Push Notification service to send push notifications to an iPhone client of your application'
+    def documentation = "http://grails.org/plugin/apns"
+    def license = 'GPL3'
+    def developers = [
+        [name: 'Arthur Neves', email: 'arthurnn@gmail.com']
     ]
-
-    def author = "Sebastien Arbogast, Arthur Neves"
-    def authorEmail = "sebastien.arbogast@gmail.com, arthurnn@gmail.com"
-    def title = "Apple Push Notification Service"
-    def description = '''\\
-This plugin allows you to integrate with Apple Push Notification service to send
-push notifications to an iPhone client of your Grails application.
-'''
-
-    // URL to the plugin's documentation
-    def documentation = "http://www.grails.org/plugin/apns"
+    def issueManagement = [url: 'https://github.com/sarbogast/grails-apns/issues']
+    def scm = [url: 'https://github.com/sarbogast/grails-apns']
 
     def doWithSpring = {
-        def configuredEnvironment
-        if(application.config.apns.environment){
-            configuredEnvironment = ApnsFactoryBean.Environment.valueOf(application.config.apns.environment.toUpperCase() as String)
-        } else {
-            switch(Environment.current){
-                case Environment.PRODUCTION: configuredEnvironment = ApnsFactoryBean.Environment.PRODUCTION
-                break
-                default: configuredEnvironment = ApnsFactoryBean.Environment.SANDBOX
+        def conf = application.config.apns
+
+        ApnsFactoryBean.Environment apnsEnvironment
+        if (conf.environment) {
+            apnsEnvironment = ApnsFactoryBean.Environment.valueOf(conf.environment.toString().toUpperCase())
+        }
+        else {
+            if (Environment.current == Environment.PRODUCTION) {
+                apnsEnvironment = ApnsFactoryBean.Environment.PRODUCTION
+            }
+            else {
+                apnsEnvironment = ApnsFactoryBean.Environment.SANDBOX
             }
         }
 
         apnsService(ApnsFactoryBean) {
-            pathToCertificate = application.config.apns.pathToCertificate ?: null
-            certificateResourcePath = application.config.apns.certificateResourcePath ?: null
-            password = application.config.apns.password
-            environment = configuredEnvironment
+            pathToCertificate = conf.pathToCertificate ?: null
+            certificateResourcePath = conf.certificateResourcePath ?: null
+            password = conf.password
+            environment = apnsEnvironment
         }
-    }
-
-    def doWithApplicationContext = {applicationContext ->
-    }
-
-    def doWithWebDescriptor = {xml ->
-    }
-
-    def doWithDynamicMethods = {ctx ->
-    }
-
-    def onChange = {event ->
-        // Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    def onConfigChange = {event ->
-        // Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
     }
 }
--- src/groovy/org/epseelon/grails/apns/ApnsFactoryBean.groovy
+++ src/groovy/org/epseelon/grails/apns/ApnsFactoryBean.groovy
@@ -1,15 +1,21 @@
 package org.epseelon.grails.apns
 
+import groovy.transform.CompileStatic
+
 import org.springframework.beans.factory.FactoryBean
+
 import com.notnoop.apns.APNS
 import com.notnoop.apns.ApnsService
+import com.notnoop.apns.ApnsServiceBuilder
 
 /**
  * @author sarbogast
  * @version 1 janv. 2010, 00:37:38
  */
+@CompileStatic
 class ApnsFactoryBean implements FactoryBean {
-    public enum Environment{SANDBOX, PRODUCTION}
+
+    static enum Environment { SANDBOX, PRODUCTION }
 
     String pathToCertificate
     String certificateResourcePath
@@ -18,38 +24,33 @@ class ApnsFactoryBean implements FactoryBean {
     Environment environment = Environment.SANDBOX
     boolean nonBlocking = false
 
-    Object getObject() {
-        def apnsService = APNS.newService()
-        if(certificateResourcePath){
-            apnsService = apnsService.withCert(this.getClass().getResourceAsStream(certificateResourcePath), password)
-        } else if(pathToCertificate){
-            apnsService = apnsService.withCert(pathToCertificate, password)
-        } else {
-            switch (environment){
-                case Environment.PRODUCTION: certificateResourcePath = "/apns-prod.p12"
-                    break
-                case Environment.SANDBOX: certificateResourcePath = "/apns-dev.p12"
+    def getObject() {
+        ApnsServiceBuilder apnsService = APNS.newService()
+        if (pathToCertificate) {
+            apnsService.withCert(pathToCertificate, password)
+        }
+        else {
+            if (!certificateResourcePath) {
+                switch (environment) {
+                    case Environment.PRODUCTION: certificateResourcePath = "/apns-prod.p12"; break
+                    case Environment.SANDBOX:    certificateResourcePath = "/apns-dev.p12"
+                }
             }
-            apnsService = apnsService.withCert(this.getClass().getResourceAsStream(certificateResourcePath), password)
+            apnsService.withCert(getClass().getResourceAsStream(certificateResourcePath), password)
         }
-		
-        switch(environment){
-            case Environment.PRODUCTION: 
-				apnsService = apnsService.withProductionDestination(); break;
-            case Environment.SANDBOX:
-            default: 
-				apnsService = apnsService.withSandboxDestination(); break;
-        }
-		
-		/*
-		 * Apns, nowadays, is just supporting Queued service. 
-		 * Was talking to the developer of the API, and he told me that furtermore
-		 * even the method asNonBlocking might be removed!
-		 */
-		  
-        apnsService = apnsService.asQueued()
 
-        return apnsService.build()
+        switch (environment) {
+            case Environment.PRODUCTION: apnsService.withProductionDestination(); break
+            case Environment.SANDBOX:    apnsService.withSandboxDestination()
+        }
+
+        /*
+         * Apns, nowadays, is just supporting Queued service.
+         * Was talking to the developer of the API, and he told me that furtermore
+         * even the method asNonBlocking might be removed!
+         */
+
+        return apnsService.asQueued().build()
     }
 
     Class getObjectType() { ApnsService }
